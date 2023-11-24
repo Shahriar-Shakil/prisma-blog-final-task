693f81260fdb802ee4ca02558606f00b67b8bbb2
shahriar-shakil <toshahriarshakil@gmail.com>
2023-11-24 15:04:38 +0600

    fix layout

--- src/components/master/HeaderDashboard.jsx
+++ src/components/master/HeaderDashboard.jsx
@@ -4,7 +4,7 @@ import ProfileDropdown from "../UI/ProfileDropdown";
 
 export default async function HeaderDashboard({ setSidebarOpen, user }) {
   return (
-    <div className="  sticky top-0 z-40 lg:mx-auto lg:max-w-7xl lg:px-8">
+    <div className="  sticky top-0 z-40 lg:mx-auto  lg:px-8">
       <div className="flex h-16 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-0 lg:shadow-none">
         <button
           type="button"
--- src/components/master/SideLayout.jsx
+++ src/components/master/SideLayout.jsx
@@ -17,9 +17,7 @@ export default async function SideLayout({ children }) {
       <div>
         <SideNavHeader user={data} />
         <main className=" lg:pl-72">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            {children}
-          </div>
+          <div className="mx-auto  px-4 sm:px-6 lg:px-8">{children}</div>
         </main>
       </div>
     </>
